5eb09097dd84ed276171c649380060e314adcc40
Devone Austin <devoneaustin@Philips-MacBook-Pro.local>
2020-05-03 18:47:12 -0700

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -41,11 +41,23 @@ class Song
     end
 
     def self.find_by_name name
-        self.all.find { |song| song.name == name }
+       self.all.find { |song| song.name == name }
     end
 
     def self.find_or_create_by_name name
-        self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+       self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+    end
+    
+    def self.new_from_filename(filename)
+        artist, song, genre = filename.split (" - ")
+        genre = genre.gsub(".mp3", "") 
+        new_artist = Artist.find_or_create_by_name(artist)
+        new_genre = Genre.find_or_create_by_name(genre)
+        self.new(song, new_artist, new_genre)
+    end
+
+    def self.create_from_filename(filename)
+        self.new_from_filename(filename).save
     end
 
 
